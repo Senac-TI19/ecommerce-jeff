eb81935d4e1fabe17e17978e40bd542f863fe925
Jefferson Martins <jeffersonmandrade@gmail.com>
2021-10-25 20:56:12 -0300

    adicionado service worker

---
+++ serviceWorker.js
@@ -0,0 +1,12 @@
+const cacheName = "cache-app-v1";
+
+const recursosCacheados = ["/", "index.html", "index.js"];
+
+self.addEventListener("install", function (event) {
+  console.log("Service Worker instalado!");
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(recursosCacheados);
+    })
+  );
+});
